64a552db33d3dbcd16aaa3532a1558db88012fbd
Deep Gaurav <deepgauravraj@gmail.com>
2021-03-19 12:23:35 +0530

    Update build_vercel.sh

--- handwriter_client/build_vercel.sh
+++ handwriter_client/build_vercel.sh
@@ -5,10 +5,7 @@ apt -y install gcc
 curl https://sh.rustup.rs -sSf | sh -s - --default-toolchain stable -y
 source ~/.cargo/env
 
-curl -L https://github.com/thedodd/trunk/releases/latest/download/trunk-x86_64-unknown-linux-gnu.tar.gz --output trunk.tar.gz
-tar -zxvf trunk.tar.gz
-
-export PATH="$PATH:$PWD"
+cargo install trunk
 
 cargo install wasm-bindgen-cli
 
