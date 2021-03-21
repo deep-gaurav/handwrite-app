ad1f946697d09250a0a4779e52bac9191297a586
Deep Gaurav <deepgauravraj@gmail.com>
2021-03-21 09:48:22 +0530

    envvar

--- .heroku/run.sh
+++ .heroku/run.sh
@@ -1,3 +1,14 @@
-ls
-echo "HANDLIBPATH $HANDLIBPATH"
+export_env_dir() {
+  env_dir=$1
+  whitelist_regex=${2:-''}
+  blacklist_regex=${3:-'^(PATH|GIT_DIR|CPATH|CPPATH|LD_PRELOAD|LIBRARY_PATH)$'}
+  if [ -d "$env_dir" ]; then
+    for e in $(ls $env_dir); do
+      echo "$e" | grep -E "$whitelist_regex" | grep -qvE "$blacklist_regex" &&
+      export "$e=$(cat $env_dir/$e)"
+      :
+    done
+  fi
+}
+export_env_dir()
 git clone https://89b81c9198c7975942f82cf05ecc040ded55051f@github.com/deep-gaurav/handwriter.git
