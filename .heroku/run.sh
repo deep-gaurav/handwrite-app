e038e36c0b3ca2924c4e7c736c6e8740092089f7
Deep Gaurav <deepgauravraj@gmail.com>
2021-03-21 14:27:13 +0530

    install requirements

--- .heroku/run.sh
+++ .heroku/run.sh
@@ -13,3 +13,4 @@ export_env_dir() {
 export_env_dir $ENV_DIR
 echo $HANDLIBPATH
 git clone https://89b81c9198c7975942f82cf05ecc040ded55051f@github.com/deep-gaurav/handwriter.git
+python -m pip install -r requirements.txt
